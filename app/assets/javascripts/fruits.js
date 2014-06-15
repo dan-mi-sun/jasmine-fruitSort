dde8e283875f2a274585ac49dbc82fa57d11d532
Dan Garland <dan@dangarland.co.uk>
2014-06-15 11:01:19 +0100

    Resetting fruits.js

--- app/assets/javascripts/fruits.js
+++ app/assets/javascripts/fruits.js
@@ -3,21 +3,5 @@
 // 1. Sortables
 
 // 2. calculatePositions
-function calculatePositions(list) {
-  _(list.childNodes).each(function(li, i) {
-    li.setAttribute("data-position", i);
-  });
-}
 
 // 3. Set positions on the box
-
-function updatePositions(data) {
-  $.ajax({
-    url: "http://localhost:3000/fruits/update_positions",
-    type: "POST",
-    data: data
-  });
-}
-
-
-
