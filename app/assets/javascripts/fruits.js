8f04ab54e29832b1b79913dc5481e5685e67084a
Daniel Sun <dan.mi.sun@gmail.com>
2014-06-16 18:10:23 +0100

    make list fruits sortable

--- app/assets/javascripts/fruits.js
+++ app/assets/javascripts/fruits.js
@@ -1,28 +1,36 @@
 // TODO
 
 // 1. Sortables
-
-// 2. calculatePositions
-function calculatePositions(list) {
-  var fruitList = list.childNodes;
-  var positions = [];
-
-  for (var i = 0; i < fruitList.length; i++) {
-
-    $(fruitList[i]).attr("data-position", i);
-    positions.push({"id":parseInt($(fruitList[i]).attr("id")),position:i});
-  }
-  return positions;
-}
-// 3. Set positions on the box
-
-
-
-function updatePositions(expectedData) {
-  $.ajax({
-    type: "POST",
-    url: "http://localhost:3000/fruits/update_positions",
-    data: JSON.stringify({ fruits: expectedData }),
-    contentType: 'application/json'
+$(document).ready(function() {
+  $("#fruits ol").sortable({
+    update: function() {
+      var positions = calculatePositions(this);
+      updatePositions(position);
+    }
+    });
   });
-};
+
+  // 2. calculatePositions
+  function calculatePositions(list) {
+    var fruitList = list.childNodes;
+    var positions = [];
+
+    for (var i = 0; i < fruitList.length; i++) {
+
+      $(fruitList[i]).attr("data-position", i);
+      positions.push({"id":parseInt($(fruitList[i]).attr("id")),position:i});
+    }
+    return positions;
+  }
+  // 3. Set positions on the box
+
+
+
+  function updatePositions(expectedData) {
+    $.ajax({
+      type: "POST",
+      url: "http://localhost:3000/fruits/update_positions",
+      data: JSON.stringify({ fruits: expectedData }),
+      contentType: 'application/json'
+    });
+  };
